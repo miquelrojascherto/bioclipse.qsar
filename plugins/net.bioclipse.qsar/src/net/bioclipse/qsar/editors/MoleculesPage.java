749fb8c5e7c04da18e04510ca276f1a7e7aac304
ospjuth <ospjuth@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2008-03-26 16:48:07 +0000

    Some small updates for forms in QSAR plugin. Nothing useful yet.
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@5187 fcb5ba71-d80d-0410-8237-ba3920747fcc

---
+++ plugins/net.bioclipse.qsar/src/net/bioclipse/qsar/editors/MoleculesPage.java
@@ -0,0 +1,62 @@
+/*******************************************************************************
+ * Copyright (c) 2008 The Bioclipse Project and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Ola Spjuth
+ *     
+ ******************************************************************************/
+package net.bioclipse.qsar.editors;
+
+import org.eclipse.jface.viewers.CheckboxTreeViewer;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.FormAttachment;
+import org.eclipse.swt.layout.FormData;
+import org.eclipse.swt.layout.FormLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.Tree;
+import org.eclipse.ui.forms.IManagedForm;
+import org.eclipse.ui.forms.editor.FormEditor;
+import org.eclipse.ui.forms.editor.FormPage;
+import org.eclipse.ui.forms.events.HyperlinkAdapter;
+import org.eclipse.ui.forms.events.HyperlinkEvent;
+import org.eclipse.ui.forms.widgets.FormToolkit;
+import org.eclipse.ui.forms.widgets.Hyperlink;
+import org.eclipse.ui.forms.widgets.ScrolledForm;
+import org.eclipse.ui.forms.widgets.Section;
+import org.eclipse.ui.forms.widgets.TableWrapData;
+import org.eclipse.ui.forms.widgets.TableWrapLayout;
+
+public class MoleculesPage extends FormPage {
+
+	private Tree tree;
+	public MoleculesPage(FormEditor editor) {
+		super(editor, "bc.molecules", "Molecules"); //$NON-NLS-1$ //$NON-NLS-2$
+	}
+
+	/**
+	 * Add content to form
+	 */
+	@Override
+	protected void createFormContent(IManagedForm managedForm) {
+
+		FormToolkit toolkit = managedForm.getToolkit();
+		ScrolledForm form = managedForm.getForm();
+		form.setText("Select molecules");
+//		form.setBackgroundImage(FormArticlePlugin.getDefault().getImage(FormArticlePlugin.IMG_FORM_BG));
+		final Composite body = form.getBody();
+
+		
+		final CheckboxTreeViewer checkboxTreeViewer = new CheckboxTreeViewer(body, SWT.NONE);
+		tree = checkboxTreeViewer.getTree();
+		toolkit.adapt(tree, true, true);
+		tree.setBounds(10, 10, 244, 326);
+	
+	}
+}
---
+++ plugins/net.bioclipse.qsar/src/net/bioclipse/qsar/editors/QSARFormEditor.java
@@ -0,0 +1,108 @@
+/*******************************************************************************
+ * Copyright (c) 2008 The Bioclipse Project and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Ola Spjuth
+ *     
+ ******************************************************************************/
+package net.bioclipse.qsar.editors;
+
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResourceChangeEvent;
+import org.eclipse.core.resources.IResourceChangeListener;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jface.dialogs.ErrorDialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.IFileEditorInput;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.editors.text.TextEditor;
+import org.eclipse.ui.forms.editor.FormEditor;
+import org.eclipse.ui.forms.widgets.ScrolledForm;
+import org.eclipse.ui.forms.widgets.TableWrapData;
+import org.eclipse.ui.forms.widgets.TableWrapLayout;
+
+public class QSARFormEditor extends FormEditor implements IResourceChangeListener, IAdaptable{
+
+	private TextEditor textEditor;
+	private MoleculesPage molPage;
+	
+	public QSARFormEditor() {
+		ResourcesPlugin.getWorkspace().addResourceChangeListener(this);
+		initialize();
+	}
+
+	/**
+	 * Do customizations based on project
+	 */
+	private void initialize() {
+		//Get project
+		if (!(getEditorInput() instanceof IFileEditorInput)) {
+			return;
+		}
+		IFileEditorInput edin = (IFileEditorInput) getEditorInput();
+		IProject project=edin.getFile().getProject();
+
+		//Get molecules folder if exists
+		IFolder molFolder=project.getFolder("molecules");
+		if (!(molFolder.exists())){
+			System.out.println("Folder 'molecules'  does not exist.");
+			//TODO: report error in some way
+			return;
+		}
+		
+	}
+
+	@Override
+	protected void addPages() {
+		molPage=new MoleculesPage(this);
+		try {
+			//Molecules page
+			addPage(molPage);
+
+			//Texteditor, should be XMLEditor: TODO
+			textEditor = new TextEditor();
+			int index = addPage(textEditor, getEditorInput());
+			setPageText(index, textEditor.getTitle());
+
+		} catch (PartInitException e) {
+			e.printStackTrace();
+		}
+	}
+
+
+
+	@Override
+	public void doSave(IProgressMonitor monitor) {
+		//TODO
+	}
+
+	@Override
+	public void doSaveAs() {
+		//TODO
+	}
+
+	@Override
+	public boolean isSaveAsAllowed() {
+		//TODO
+		return false;
+	}
+	
+	@Override
+	public void dispose() {
+		ResourcesPlugin.getWorkspace().removeResourceChangeListener(this);
+		super.dispose();
+	}
+
+	public void resourceChanged(IResourceChangeEvent event) {
+		//TODO
+	}
+
+}
