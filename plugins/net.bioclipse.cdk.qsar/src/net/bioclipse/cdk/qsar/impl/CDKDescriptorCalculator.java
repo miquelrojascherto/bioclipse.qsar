a687ddb9a8a832791af6c6d5920bd7db168960bf
ospjuth <ospjuth@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2008-08-19 16:05:21 +0000

    Skeleton for CDK implementation of descriptor EP.
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@6650 fcb5ba71-d80d-0410-8237-ba3920747fcc

---
+++ plugins/net.bioclipse.cdk.qsar/src/net/bioclipse/cdk/qsar/impl/CDKDescriptorCalculator.java
@@ -0,0 +1,19 @@
+package net.bioclipse.cdk.qsar.impl;
+
+import java.util.List;
+import java.util.Map;
+
+import net.bioclipse.core.domain.IMolecule;
+import net.bioclipse.qsar.descriptor.IDescriptorCalculator;
+import net.bioclipse.qsar.descriptor.IDescriptorResult;
+
+public class CDKDescriptorCalculator implements IDescriptorCalculator {
+
+	public Map<IMolecule, IDescriptorResult> calculateDescriptor(
+			List<IMolecule> molecules, String[] descriptorIDs) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+}
