018b03a2ded0d7e5c563607b868d2a468adf154a
ospjuth <ospjuth@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2009-02-26 10:41:47 +0000

    More extensible logging on preference initialization.
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@9076 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.qsar/src/net/bioclipse/qsar/prefs/QSARPreferenceInitializer.java
+++ plugins/net.bioclipse.qsar/src/net/bioclipse/qsar/prefs/QSARPreferenceInitializer.java
@@ -1,14 +1,17 @@
 package net.bioclipse.qsar.prefs;
 
 import net.bioclipse.qsar.QSARConstants;
+import net.bioclipse.qsar.business.QsarManager;
 import net.bioclipse.qsar.init.Activator;
 
+import org.apache.log4j.Logger;
 import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
 import org.eclipse.core.runtime.preferences.DefaultScope;
 import org.eclipse.core.runtime.preferences.IEclipsePreferences;
 
 public class QSARPreferenceInitializer extends AbstractPreferenceInitializer {
 
+    private static final Logger logger = Logger.getLogger(QSARPreferenceInitializer.class);
 
 	public QSARPreferenceInitializer() {
 		super();
@@ -19,12 +22,16 @@ public class QSARPreferenceInitializer extends AbstractPreferenceInitializer {
 
     	//Read available providers from EP
 		String defstr=QsarPreferenceHelper.getAvailableProvidersFromEP();
+		
+		logger.debug( "Read availiable descriptors from ExPo: " + defstr );
 
 		IEclipsePreferences prefs = new DefaultScope().getNode(Activator.PLUGIN_ID);
 		prefs.put(
 				QSARConstants.QSAR_PROVIDERS_ORDER_PREFERENCE, 
 				defstr);
-    	
+
+    logger.debug( "Set qsar providers order prefs to: " + defstr );
+
     }
     
     
