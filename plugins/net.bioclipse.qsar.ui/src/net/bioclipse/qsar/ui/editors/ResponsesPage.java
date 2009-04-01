5290743d87b7d73fe08a4476c19a818267e64144
ospjuth <ospjuth@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2009-04-01 15:25:56 +0000

    Fixed ResponsesPage.
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@9549 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.qsar.ui/src/net/bioclipse/qsar/ui/editors/ResponsesPage.java
+++ plugins/net.bioclipse.qsar.ui/src/net/bioclipse/qsar/ui/editors/ResponsesPage.java
@@ -25,11 +25,16 @@ import net.bioclipse.qsar.QsarType;
 import net.bioclipse.qsar.ResourceType;
 import net.bioclipse.qsar.ResponseType;
 import net.bioclipse.qsar.ResponsesListType;
+import net.bioclipse.qsar.StructureType;
 
 import org.apache.log4j.Logger;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.emf.common.command.Command;
+import org.eclipse.emf.common.command.CompoundCommand;
 import org.eclipse.emf.common.ui.viewer.IViewerProvider;
+import org.eclipse.emf.edit.command.DeleteCommand;
+import org.eclipse.emf.edit.command.RemoveCommand;
 import org.eclipse.emf.edit.command.SetCommand;
 import org.eclipse.emf.edit.domain.EditingDomain;
 import org.eclipse.emf.edit.domain.IEditingDomainProvider;
@@ -71,41 +76,41 @@ public class ResponsesPage extends FormPage implements IEditingDomainProvider, I
     private Table responsesTable;
 
     private static final Logger logger = Logger.getLogger(MoleculesPage.class);
-    
+
     ICDKManager cdk;
     DecimalFormat formatter;
-    
+
     private QsarEditorSelectionProvider selectionProvider;
-	private EditingDomain editingDomain;
+    private EditingDomain editingDomain;
 
-	private QsarType qsarModel;
-	private ResponsesListType responsesList;
+    private QsarType qsarModel;
+    private ResponsesListType responsesList;
 
 
-    
-	public ResponsesPage(FormEditor editor, QsarType qsarModel, 
-			EditingDomain editingDomain, QsarEditorSelectionProvider selectionProvider) {
 
-		super(editor, "qsar.responses", "Responses");
-		this.editingDomain=editingDomain;
+    public ResponsesPage(FormEditor editor, QsarType qsarModel, 
+                         EditingDomain editingDomain, QsarEditorSelectionProvider selectionProvider) {
 
-		//Get Managers via OSGI
-		cdk=Activator.getDefault().getCDKManager();
+        super(editor, "qsar.responses", "Responses");
+        this.editingDomain=editingDomain;
 
-		this.qsarModel = qsarModel;
+        //Get Managers via OSGI
+        cdk=Activator.getDefault().getCDKManager();
 
-		formatter = new DecimalFormat("0.00");
+        this.qsarModel = qsarModel;
+
+        formatter = new DecimalFormat("0.00");
         this.selectionProvider=selectionProvider;
-        
+
         this.responsesList=qsarModel.getResponselist();
         if (responsesList==null){
-        	responsesList=QsarFactory.eINSTANCE.createResponsesListType();
-        	qsarModel.setResponselist(responsesList);
+            responsesList=QsarFactory.eINSTANCE.createResponsesListType();
+            qsarModel.setResponselist(responsesList);
         }
 
         editor.addPageChangedListener(this);
-        
-	}
+
+    }
 
 
     /**
@@ -118,104 +123,117 @@ public class ResponsesPage extends FormPage implements IEditingDomainProvider, I
         FormToolkit toolkit = managedForm.getToolkit();
         form.setText("QSAR responses");
         toolkit.decorateFormHeading(form.getForm());
-        
-		IProject project=((QsarEditor)getEditor()).getActiveProject();
+
+        IProject project=((QsarEditor)getEditor()).getActiveProject();
         ToolbarHelper.setupToolbar(form, project);
-        
+
         GridLayout layout = new GridLayout();
         layout.numColumns=2;
         form.getBody().setLayout(layout);
 
         responsesViewer = new TableViewer(form.getBody(), SWT.BORDER
-        		| SWT.FULL_SELECTION );
+                                          | SWT.FULL_SELECTION );
         responsesTable=responsesViewer.getTable();
         GridData gd=new GridData(GridData.FILL_VERTICAL);
         gd.widthHint=400;
         responsesTable.setLayoutData( gd );
-        
+
         responsesTable.setHeaderVisible(true);
         toolkit.adapt(responsesTable, true, true);
-        
+
         //Add providers columns
         TableLayout tableLayout = new TableLayout();
         responsesTable.setLayout(tableLayout);
-        
+
         TableViewerColumn molCol=new TableViewerColumn(responsesViewer, SWT.NONE);
         molCol.getColumn().setText("ID");
         tableLayout.addColumnData(new ColumnPixelData(250));
         molCol.setLabelProvider(new ColumnLabelProvider(){
-        	@Override
-        	public String getText(Object element) {
-        		ResponseType response = (ResponseType)element;
-        		return response.getStructureID();
-        	}
+            @Override
+            public String getText(Object element) {
+                ResponseType response = (ResponseType)element;
+                return response.getStructureID();
+            }
         });
 
         TableViewerColumn responseCol=new TableViewerColumn(responsesViewer, SWT.NONE);
         responseCol.getColumn().setText("Response");
         tableLayout.addColumnData(new ColumnPixelData(100));
-        
-    	responseCol.setLabelProvider(new ColumnLabelProvider(){
-    		@Override
-    		public String getText(Object element) {
-    			ResponseType response = (ResponseType)element;
-          if (response.getArrayValues()!=null && response.getArrayValues().length()>0)
-					return response.getArrayValues();
-				else
-					if (Float.isNaN(response.getValue()))
-						return "";
-					else
-						return ""+response.getValue();
-    		}
-    	});
-        
-        responseCol.setEditingSupport(new EditingSupport(responsesViewer){
-    		private TextCellEditor cellEditor;
-    		
-			@Override
-			protected boolean canEdit(Object element) {
-				return true;
-			}
 
-			@Override
-			protected CellEditor getCellEditor(Object element) {
-				if (cellEditor==null) cellEditor=new TextCellEditor(responsesTable);
-				return cellEditor;
-			}
-
-			@Override
-			protected Object getValue(Object element) {
-				ResponseType response = (ResponseType)element;
-				if (response.getArrayValues()!=null && response.getArrayValues().length()>0)
-					return response.getArrayValues();
-				else{
-					if (Float.isNaN(response.getValue()))
-						return "";
-					else
-						return ""+response.getValue();
-				}
-			}
-
-			@Override
-			protected void setValue(Object element, Object value) {
-				ResponseType response = (ResponseType)element;
-				try{
-					float f=Float.parseFloat(String.valueOf(value));
-					SetCommand cmd=new SetCommand(editingDomain,response,QsarPackage.Literals.RESPONSE_TYPE__VALUE,f);
-					editingDomain.getCommandStack().execute(cmd);
-				}catch (NumberFormatException e){
-					String str=String.valueOf(value);
-					SetCommand cmd=new SetCommand(editingDomain,response,QsarPackage.Literals.RESPONSE_TYPE__ARRAY_VALUES,str);
-					editingDomain.getCommandStack().execute(cmd);
-				}
-				responsesViewer.refresh();
-			}
-        	
+        responseCol.setLabelProvider(new ColumnLabelProvider(){
+            @Override
+            public String getText(Object element) {
+                ResponseType response = (ResponseType)element;
+                if (response.getArrayValues()!=null && response.getArrayValues().length()>0)
+                    return response.getArrayValues();
+                else
+                    if (Float.isNaN(response.getValue()))
+                        return "";
+                    else
+                        return ""+response.getValue();
+            }
         });
-        
-        
+
+        responseCol.setEditingSupport(new EditingSupport(responsesViewer){
+            private TextCellEditor cellEditor;
+
+            @Override
+            protected boolean canEdit(Object element) {
+                return true;
+            }
+
+            @Override
+            protected CellEditor getCellEditor(Object element) {
+                if (cellEditor==null) cellEditor=new TextCellEditor(responsesTable);
+                return cellEditor;
+            }
+
+            @Override
+            protected Object getValue(Object element) {
+                ResponseType response = (ResponseType)element;
+                if (response.getArrayValues()!=null && response.getArrayValues().length()>0)
+                    return response.getArrayValues();
+                else{
+                    if (Float.isNaN(response.getValue()))
+                        return "";
+                    else
+                        return ""+response.getValue();
+                }
+            }
+
+            @Override
+            protected void setValue(Object element, Object value) {
+                ResponseType response = (ResponseType)element;
+                try{
+                    float f=Float.parseFloat(String.valueOf(value));
+                    CompoundCommand ccmd=new CompoundCommand();
+                    Command cmd=new SetCommand(editingDomain,response,QsarPackage.Literals.RESPONSE_TYPE__VALUE,f);
+                    ccmd.append( cmd );
+                    //also remove any previous ARRAY_VALUES
+//                    response.getArrayValues();
+//                    cmd=new RemoveCommand(editingDomain,response,QsarPackage.Literals.RESPONSE_TYPE__ARRAY_VALUES,"");
+//                    ccmd.append( ccmd );
+                    
+                    editingDomain.getCommandStack().execute(ccmd);
+                }catch (NumberFormatException e){
+                    String str=String.valueOf(value);
+                    CompoundCommand ccmd=new CompoundCommand();
+                    Command cmd=new SetCommand(editingDomain,response,QsarPackage.Literals.RESPONSE_TYPE__ARRAY_VALUES,str);
+                    ccmd.append( cmd );
+                    //Also remove any previous VALUE
+//                    cmd=new RemoveCommand(editingDomain,response,QsarPackage.Literals.RESPONSE_TYPE__VALUE,"");
+//                    ccmd.append( cmd );
+                    
+                    editingDomain.getCommandStack().execute(ccmd);
+                }
+                responsesViewer.refresh();
+            }
+
+        });
+
+
         responsesViewer.setContentProvider(new ArrayContentProvider());
-//        responsesViewer.setLabelProvider(new DescriptorLabelProvider());
+        //        responsesViewer.setLabelProvider(new DescriptorLabelProvider());
 
         //Sort by name
         responsesViewer.setSorter(new ResponseSorter());
@@ -226,13 +244,13 @@ public class ResponsesPage extends FormPage implements IEditingDomainProvider, I
                 if (e.keyCode==SWT.DEL){
                     deleteSelectedResponses();
                 }
-                
+
                 //Space key, toggle selection
                 if (e.keyCode==32){
 
-              	  IStructuredSelection msel=(IStructuredSelection) responsesViewer.getSelection();
+                    IStructuredSelection msel=(IStructuredSelection) responsesViewer.getSelection();
                     //TODO: implement
-                    
+
                 }
             }
             public void keyReleased( KeyEvent e ) {
@@ -244,96 +262,73 @@ public class ResponsesPage extends FormPage implements IEditingDomainProvider, I
             public void handleEvent(Event e) {
                 importResponses();
             }
-          });
+        });
         GridData gda2=new GridData(GridData.VERTICAL_ALIGN_BEGINNING);
         gda2.widthHint=100;
         btnAdd.setLayoutData( gda2 );
-        
+
         toolkit.paintBordersFor(form);
-        
+
         synchronizeResponesWithModel();
 
-		//Populate selected descriptors from the read qsar model 
-//		populateResponsesViewerFromModel();
+        //Populate selected descriptors from the read qsar model 
+        //		populateResponsesViewerFromModel();
         if (responsesList.eContents()!=null){
-    		responsesViewer.setInput(responsesList.eContents().toArray());
+            responsesViewer.setInput(responsesList.eContents().toArray());
         }
 
         //Post selections to Eclipse via our intermediate selectionprovider
         selectionProvider.setSelectionProviderDelegate( responsesViewer );
-//        responsesViewer.getTable().setFocus();
+        //        responsesViewer.getTable().setFocus();
     }
 
-    
-	private void synchronizeResponesWithModel() {
 
-		List<ICDKMolecule> allMolecules=new ArrayList<ICDKMolecule>();
-		
-		//Go through all molecules in moleculeResources and make sure they have a response
-		for (ResourceType molres : qsarModel.getStructurelist().getResources()){
+    /**
+     * Add a response for every structure.
+     */
+    private void synchronizeResponesWithModel() {
 
-			String structID="";
-			
-			//Only work with files for now. TODO: include URL:s 
-			try {
-				List<ICDKMolecule> mols = cdk.loadMolecules(molres.getFile());
-				for (ICDKMolecule mol : mols){
-					
-					//Add to list of total mols
-					allMolecules.add(mol);
+        List<ICDKMolecule> allMolecules=new ArrayList<ICDKMolecule>();
 
-					boolean hasResponse=false;
-					
-					//Does this one have a response already?
-					for (ResponseType response : responsesList.getResponse()){
-						if (response.getStructureID().equals(molres.getId())){
-							if (structID.equals( response.getStructureID() )){
-								logger.debug("Found an existing response for mol: " + mol.getName());
-								hasResponse=true;
-							}
-						}
-					}
-					
-					if (!hasResponse){
-						//Create a new response
-						ResponseType newResponse=QsarFactory.eINSTANCE.createResponseType();
-						newResponse.setStructureID( molres.getId());
-//						newResponse.setResourceIndex(structID);
-						newResponse.setValue(Float.NaN);
-						responsesList.getResponse().add(newResponse);
-						//Do not use command, this will fire dirty.
-						//Keep silent, if things do not change we will recreate
-						//these the next time.
-					}
-					
-					//TODO: remove any responses that refers to a non-existing
-					//molecularResource (on resource removal).
-					
-					//Get next mol
-//					structID++;//FIXME
+        //Loop over all resources
+        for (ResourceType molres : qsarModel.getStructurelist().getResources()){
 
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			} catch (BioclipseException e) {
-				e.printStackTrace();
-			} catch (CoreException e) {
-				e.printStackTrace();
-			}
-			
-		}
-		
-		
-	}
+            //Loop over all structures in a resource
+            for (StructureType structure : molres.getStructure()){
+
+                boolean hasResponse=false;
+
+                //Does this structure have a response already?
+                for (ResponseType response : responsesList.getResponse()){
+                    if (structure.getId().equals(response.getStructureID())){
+                        logger.debug("Found an existing response for structure: " + structure.getId());
+                        hasResponse=true;
+                    }
+                }
+
+                if (!hasResponse){
+                    //Create a new response and add to structure
+                    ResponseType newResponse=QsarFactory.eINSTANCE.createResponseType();
+                    newResponse.setStructureID( structure.getId());
+                    newResponse.setValue(Float.NaN);
+                    responsesList.getResponse().add(newResponse);
+                    //Do not use command, this will fire dirty.
+                    //Keep silent, if things do not change we will recreate
+                    //these the next time.
+                }
+            }
+        }
+
+    }
 
 
-	private void populateResponsesViewerFromModel() {
+    private void populateResponsesViewerFromModel() {
 
-		
-		
-		responsesViewer.setInput(responsesList.eContents().toArray());
-		
-/*		
+
+
+        responsesViewer.setInput(responsesList.eContents().toArray());
+
+        /*		
 		// The content provider is responsible to handle add and
 		// remove notification for the Person#address EList
 		ObservableListContentProvider provider = new ObservableListContentProvider();
@@ -353,46 +348,46 @@ public class ResponsesPage extends FormPage implements IEditingDomainProvider, I
 		// Person#addresses is the Viewer's input
 		responsesViewer.setInput(EMFEditObservables.observeList(Realm.getDefault(), editingDomain, descriptorList,
 			QsarPackage.Literals.DESCRIPTORLIST_TYPE__DESCRIPTOR));
-*/
-	}
+         */
+    }
 
 
-	protected void importResponses() {
-		
-		showMessage("Not implemented yet");
-		
-	}
+    protected void importResponses() {
+
+        showMessage("Not implemented yet");
+
+    }
 
 
-	/**
+    /**
      * Handle the case when users press the Remove button next to moleculeviewer
      * or presses the delete button on something
      */
     protected void deleteSelectedResponses() {
-    	
-    	System.out.println("Delete responses not implemented");
-    	
-/*    	
+
+        System.out.println("Delete responses not implemented");
+
+        /*    	
     	IStructuredSelection ssel=(IStructuredSelection) rightViewer.getSelection();
-		
+
     	CompoundCommand ccmd=new CompoundCommand();
 
     	//Collect commands from selection
     	for (Iterator<?> it=ssel.iterator(); it.hasNext();){
 
     		Object obj = it.next();
-    		
+
     		if (obj instanceof DescriptorType) {
 				DescriptorType descType = (DescriptorType) obj;
 				Command cmd=RemoveCommand.create(editingDomain, descriptorList, QsarPackage.Literals.DESCRIPTORLIST_TYPE__DESCRIPTOR, descType);
 				ccmd.append(cmd);
 			}
-    		
+
     	}
 
 		//Execute the commands
 		editingDomain.getCommandStack().execute(ccmd);
-*/
+         */
     }
 
     private void showMessage(String message) {
@@ -400,27 +395,35 @@ public class ResponsesPage extends FormPage implements IEditingDomainProvider, I
                                        "Information",
                                        message );
     }
-    
-    
-    
 
-	public void activatePage() {
+
+
+
+    public void activatePage() {
 
         selectionProvider.setSelectionProviderDelegate( responsesViewer );
+        synchronizeResponesWithModel();
+        
+        if (responsesList.eContents()!=null){
+            responsesViewer.setInput(responsesList.eContents().toArray());
+        }
+
+        responsesViewer.refresh();
+        
     }
 
     public class Stopwatch {
         private long start;
         private long stop;
-        
+
         public void start() {
             start = System.currentTimeMillis(); // start timing
         }
-        
+
         public void stop() {
             stop = System.currentTimeMillis(); // stop timing
         }
-        
+
         public long elapsedTimeMillis() {
             return stop - start;
         }
@@ -431,22 +434,22 @@ public class ResponsesPage extends FormPage implements IEditingDomainProvider, I
         }
     }
 
-	public EditingDomain getEditingDomain() {
-		return editingDomain;
-	}
+    public EditingDomain getEditingDomain() {
+        return editingDomain;
+    }
 
 
-	public Viewer getViewer() {
-		return responsesViewer;
-	}
+    public Viewer getViewer() {
+        return responsesViewer;
+    }
 
-	public void pageChanged(PageChangedEvent event) {
+    public void pageChanged(PageChangedEvent event) {
 
-		if (event.getSelectedPage()!=this) return;
-		
-		activatePage();
-		
-	}
+        if (event.getSelectedPage()!=this) return;
+
+        activatePage();
+
+    }
 
 
 }
